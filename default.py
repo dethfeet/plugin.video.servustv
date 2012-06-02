bce25cf031f26ad32ac4a050f23f6c4c99c29095
dethfeet <dethfeet@qip.ru>
2012-06-02 10:27:15 +0200

    Added search function + fixed "all"

--- default.py
+++ default.py
@@ -13,11 +13,10 @@ addon = xbmcaddon.Addon(id='plugin.video.servustv')
 thisPlugin = int(sys.argv[1])
 baseLink = "http://www.servustv.com/"
 
-mediathekLink = baseLink + "cs/Satellite/VOD-Mediathek/001259088496198?p=1259088496182"
+mediathekLink = baseLink + "cs/Satellite/VOD-Mediathek/001259088496198"
 showLink = baseLink + "cs/Satellite?categoryId=%s&categoryNodeId=%s&pagename=ServusTV/Ajax/MediathekData&nachThemen=all&nachSendung=%s&nachThemenNodeId=null&nachThemen_changed=1&nachSendung_changed=2&ajax=true"
+searchLink = baseLink + "cs/Satellite?cid=&currentPage=1&pagename=servustv/Page/SearchResultsRenderer&searchkey=%s&tabType=video"
 
-#cs/Satellite?categoryId=1259088496213&categoryNodeId=1259088496215&pagename=ServusTV%2FAjax%2FMediathekData&nachThemen=all&nachSendung=all&nachThemenNodeId=null&nachThemen_changed=1&nachSendung_changed=2&ajax=true
-#cs/Satellite?categoryId=1259088496213&categoryNodeId=1259088496215&pagename=ServusTV%2FAjax%2FMediathekData&nachThemen=all&nachSendung=1259406665024&nachThemenNodeId=null&nachThemen_changed=1&nachSendung_changed=2&ajax=true
 height = 1080
 const = "8e99dff8de8d8e378ac3f68ed404dd4869a4c007"
 playerID = 1254928709001
@@ -25,25 +24,24 @@ publisherID = 900189268001
 
 playerKey = "AQ~~,AAAA0Zd2KCE~,a1ZzPs5ODGffVvk2dn1CRCof3Ru_I9gE"
 
-_regex_extractCategories = re.compile("<li class='category[0-9]{1}'><a href='(.*?)'>\r\n(.*?)</a></li>", re.DOTALL)
+_regex_extractCategories = re.compile("<li class=(\"allevideos\"|'category[0-9]{1}')><a href='(.*?)'>(.*?)</a></li>", re.DOTALL)
 _regex_extractShows = re.compile("<select name=\"nachSendung\" id=\"nachSendung\">.*?</select>", re.DOTALL)
 _regex_extractShow = re.compile("<option value='(.*?)'>(.*?)</option>")
 
 
 _regex_extractShowNext = re.compile("<li><a href='(.*?)' class=\"nachste\">.*?</a></li>")
 
-_regex_extractEpisode = re.compile("<li class='.*?'>.*?<a href='#' title='(.*?)'.*?<img src='(.*?)'.*?name=\"videoList\.subcategory\" value='([0-9]*)'/>.*?Sendung vom ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}).*?<div class=\"programDescription\">(.*?)</div>.*?</li>", re.DOTALL)
+_regex_extractEpisode = re.compile("<!-- videotype:vod -->[ \n\r\t]*<li class='.*?'>.*?<a href='#' title='(.*?)'.*?<img src='(.*?)'.*?name=\"videoList\.subcategory\" value='([0-9]*)'/>.*?Sendung vom ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}).*?(<div class=\"programDescription\">|<span class=\"description\">)(.*?)(</div>|</span>).*?</li>", re.DOTALL)
 
 def mainPage():
     page = load_page(mediathekLink)
     
-    menu_link = showLink % ("","","")
-    menu_name = "Alle"
-    addDirectoryItem(menu_name, {"action" : "show", "link": menu_link})
+    menu_name = "Suche"
+    addDirectoryItem(menu_name, {"action" : "search"})
     
     for category in _regex_extractCategories.finditer(page):
-        menu_link = category.group(1)
-        menu_name = category.group(2)
+        menu_link = category.group(2)
+        menu_name = category.group(3)
         addDirectoryItem(menu_name, {"action" : "category", "link": menu_link})
     xbmcplugin.endOfDirectory(thisPlugin)
 
@@ -55,18 +53,17 @@ def showCategory(link):
     menu_link = showLink % (categoryId,"","")
     menu_name = "Alle"
     addDirectoryItem(menu_name, {"action" : "show", "link": menu_link})
-    
-    
+
     shows = _regex_extractShows.search(page)
     
     for show in _regex_extractShow.finditer(shows.group(0)):
         menu_link = showLink % (categoryId,"",show.group(1))
         menu_name = show.group(2)
-        addDirectoryItem(menu_name, {"action" : "show", "link": menu_link})
+        addDirectoryItem(menu_name.strip(), {"action" : "show", "link": menu_link})
     xbmcplugin.endOfDirectory(thisPlugin)
 
-def showPage(link):
-    page = load_page(urllib.unquote(link))
+def showPage(link=None):
+    page = load_page(link)
     
     for episode in _regex_extractEpisode.finditer(page):
         date = episode.group(4)
@@ -93,6 +90,19 @@ def playEpisode(videoPlayer):
         
     item = xbmcgui.ListItem(path=finalurl)
     xbmcplugin.setResolvedUrl(thisPlugin, True, item)
+
+def searchVideo():
+    keyboard = xbmc.Keyboard("")    
+    keyboard.doModal();
+    searchString = keyboard.getText()
+    searchString = searchString.strip()
+    searchString = urllib.quote_plus(searchString)
+    
+    if searchString == "":
+        return False
+    
+    searchUrl = searchLink % (searchString)
+    showPage(searchUrl)
 
 def load_page(url):
     print url
@@ -135,8 +145,10 @@ else:
     if params['action'] == "category":
         showCategory(params['link'])
     elif params['action'] == "show":
-        showPage(params['link'])
+        showPage(urllib.unquote(params['link']))
     elif params['action'] == "episode":
         playEpisode(params['link'])
+    elif params['action'] == "search":
+        searchVideo()
     else:
         mainPage()
